a6921691b02a279067a2a5c2beffbdeb0a62571e
fong123123123 <darber.fong@gmail.com>
2023-09-24 19:04:59 +0800

    test

--- electric_forecast.R
+++ electric_forecast.R
@@ -6,7 +6,7 @@ data()
 data <- ts(data[,2], frequency = 12, start = c(1985,1))
 plot(data, main = "Time Series Plot", xlab = "Time", ylab = "Value")
 
-
+#train test split
 train <- head(data, round(length(data) * 0.70))
 h <- length(data) - length(train)
 test <- tail(data, h)
@@ -14,3 +14,24 @@ train
 test
 autoplot(train) + autolayer(test)
 
+#log transform
+data_transform <- log(data)
+data_transform <- ts(data_transform, frequency = 12, start = c(1985,1))
+plot(data_transform, main = "Time Series Plot", xlab = "Time", ylab = "Value")
+
+#auto arima model
+arima_model <- auto.arima(data)
+summary(arima_model)
+
+#residuals of auto arima model
+residuals <- residuals(arima_model)
+residuals
+
+#acf of the residuals
+acf_residuals <-acf(residuals, main="ACF of Residuals")
+ACF$acf_residuals
+
+
+#correlogram ACF
+acf <- acf(data, main="Correlogram for the Electric Dataset")
+ACF$acf
