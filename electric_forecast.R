fd6c4ba1994191f0e0579c8c6572138c63b8bb23
roy2003teh <142188932+roy2003teh@users.noreply.github.com>
2023-09-25 23:26:52 +0800

    Add files via upload

--- electric_forecast.R
+++ electric_forecast.R
@@ -17,6 +17,11 @@ train
 test
 autoplot(train) + autolayer(test)
 
+#Step 1 Decomposition
+decomposed_data <- decompose(train)
+# Plot the decomposed components
+plot(decomposed_data)
+
 #Step 2
 #transformation
 data_transform <- log(data)
@@ -27,28 +32,48 @@ plot(data_transform, main = "Time Series Plot (Log Transform)", xlab = "Time", y
 #Differencing
 # First-order differencing
 diff_data <- diff(data_transform)
+seasonalDiff <- diff(data_transform, lag = 12)
 
 # Plot differenced data
 plot(diff_data, main = "Differenced Time Series", xlab = "Time", ylab = "Differences")
+plot(seasonalDiff)
 
 #Step 3b Check the stationarity of the series
 adf.test(diff_data)
+adf.test(train)
 
 #Step 4
-#auto arima model
-arima_model <- auto.arima(train)
-summary(arima_model)
-
 #residuals of auto arima model
 residuals <- residuals(arima_model)
 residuals
 
-#acf of the residuals
+#PACF && Acf of the residuals
 acf_residuals <-acf(residuals, main="ACF of Residuals")
-ACF$acf_residuals
+pacf_residuals <-pacf(residuals, main = "PACF of Residuals")
 
-
-#ACF
+#PACF && ACF of Original Data
 acf <- acf(train, main="Correlogram for the Electric Dataset")
-ACF$acf
+pacf <- pacf(train, main = "Autocorrelogram")
+
+#Step 6 Check residual
+#Box-Pierce Q 
+Box.test(residuals)
+
+#Differencing
+
+#Step 7
+#Arima model
+#arima_model <- arima(train, c(2,0,1), order = c(0,1,1), 12)
+arima_model <- arima(train, order = c(2, 0, 1), seasonal = list(order = c(0, 1, 1), period = 12))
+summary(arima_model)
+
+#accuracy(train)
+
+#ARIMA using auto.arima
+arima_model <- auto.arima(train, ic= "aic", trace = TRUE)
+summary(arima_model)
+
+#Step 8 Form Equation for the Best Model
+
+
 
