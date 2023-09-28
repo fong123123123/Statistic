1bc2a5d48fca680609dcc5d44b6314233adb55b4
0001 darber <fongws-wm20@student.tarc.edu.my>
2023-09-28 15:34:40 +0800

    differencing

--- electric_forecast_1.R
+++ electric_forecast_1.R
@@ -6,31 +6,35 @@ library(lmtest)
 
 #Step 0
 data <- read.csv("Electric_Production.csv")
-data()
+data <- ts(data[,2], frequency = 12, start = c(1985,1))
+
+
 
 #train test split
 train <- head(data, round(length(data) * 0.70))
 h <- length(data) - length(train)
 test <- tail(data, h)
-train
-test
 autoplot(train) + autolayer(test)
 
 #======================================================
 #Step 1 (Visualise Time Series)
 #plot data 
-data <- ts(data[,2], frequency = 12, start = c(1985,1))
 plot(data, main = "Time Series Plot", xlab = "Time", ylab = "Value")
 
 #Decomposition of Time Series
-decomposed_data <- decompose(data)
+decomposed_data <- decompose(train)
 plot(decomposed_data)
+acf(train)
+pacf(train)
+
 
 # Analysis for Residual
-test <- decomposed_data$random
-summary(decomposed_data$random)
-acf(test, main = "ACF of Residual Component")
-pacf(test, main = "PACF of Residual Component")
+#decomposed_data$random <- na.omit(decomposed_data$random)
+#summary(decomposed_data$random)
+#acf(decomposed_data$random, main = "ACF of Residual Component")
+#pacf(decomposed_data$random, main = "PACF of Residual Component")
+
+
 
 #======================================================
 #Step 2 (Transformation)
@@ -41,42 +45,51 @@ plot(train, main = "Time Series Plot (Log Transform)", xlab = "Time", ylab = "Va
 
 #======================================================
 #Step 3 (Stationarize the Series)
-#Check Stationarity
-adf.test(train)
+#seasonal diff
+seasonalDiff <- diff(train, lag= 12)
+plot(seasonalDiff)
+acf(seasonalDiff)
+pacf(seasonalDiff)
 
+#Check Non-Seasonal Stationarity
+adf.test(seasonalDiff)
+
+train <- seasonalDiff
 
 #======================================================
 #Step 4 (ETS, Arima model)
 #ETS model
-ets_model_addictive <- ets(train, model = "AAA")
-ets_model_multiplicative <- ets(train, model = "MMM")
-ets_model_mixed <- ets(train, model = "ZZZ")
+ets_model <- ets(train)
 #Arima model
 arima_model <- arima(train, order = c(2, 0, 1), seasonal = list(order = c(0, 1, 1), period = 12))
 auto_arima_model <- auto.arima(train)
+
 #Holt-Winters model
 hw_model_additive <- HoltWinters(train, seasonal = "additive")
 
-
+#summary 
+summary(ets_model)
+summary(arima_model)
+summary(auto_arima_model)
 
 #======================================================
 #Step 6 (Diagnotic Checking)
-checkresiduals(ets_model_addictive)
-checkresiduals(ets_model_multiplicative)
-checkresiduals(ets_model_mixed)
+checkresiduals(ets_model)
 checkresiduals(arima_model)
 checkresiduals(auto_arima_model)
+checkresiduals(hw_model_additive)
 
 #======================================================
 #Step 7 (Determine Best Model)
+accuracy(ets_model)
 accuracy(arima_model)
 accuracy(auto_arima_model)
-accuracy(ets_model_addictive)
-accuracy(ets_model_multiplicative)
-accuracy(ets_model_mixed)
+accuracy(hw_model_additive)
+
 
 #======================================================
 #Step 8a (Form Equation for Best Model)
+
 
 #======================================================
 #Step 8b (Estimate model coefficient)
@@ -84,7 +97,7 @@ arima(data, order = c(0,1,1), seasonal = list(order=c(0,1,1), period = 12))
 
 #======================================================
 #Step 8c (Test significance of the coefficient)
-fit <- arima(data, order = c(0,1,1), seasonal = list(order=c(0,1,1), period = 12))
-coeftest(fit)
+#fit <- arima(data, order = c(0,1,1), seasonal = list(order=c(0,1,1), period = 12))
+coeftest(auto_arima_model)
 
 #======================================================
